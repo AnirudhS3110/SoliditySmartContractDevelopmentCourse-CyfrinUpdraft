de4d1ac0b604c045d481713da7d24e362e277fa0
AnirudhS3110 <anirudh23bcs19@iiitkottayam.ac.in>
2025-11-12 21:46:20 +0530

    "First practice session in Solidity, deployed to ZkSync Remote Dev net and ethereum test net"

---
+++ contracts/1_Storage.sol
@@ -0,0 +1,28 @@
+pragma solidity ^0.8.18;
+
+contract SimpleStorage {
+    uint256 favouriteNumber;
+
+    struct Person {
+        uint256 favNumber;
+        string name;
+    } 
+    Person public person1 = Person(7 , "Bob");
+
+    Person[] list_of_people;
+    function store(uint _fav) public {
+        favouriteNumber = _fav;
+    }
+
+    mapping(string => uint256) public nameToFav;
+
+    function retrieve() public view returns  (uint256) {
+        return favouriteNumber;
+    }
+
+    function addPerson(uint256 _favNumber , string memory _name) public {
+        list_of_people.push(Person(_favNumber,_name));
+        nameToFav[_name] = _favNumber;
+    }
+}
+
